dd7a24552837fa425d1b405cf34226598764004d
sefyudev <sefyusefyu17@gmail.com>
2025-05-29 11:18:58 +0100

    fix login sharing doc

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const folderController = require("../controllers/folderController");
-const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/auth");
 
 // All routes are protected
 router.use(protect);
